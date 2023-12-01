e6c2bdb2d563b3f42085a1222033cfb303e52c68
DillDall <t.a.dahll@gmail.com>
2023-12-02 00:33:29 +0100

    Added regex solution. Works. - Had to brain out overlapping match pattern, thanks gpt.

---
+++ day1/day1-2solution.go
@@ -0,0 +1,77 @@
+package day1
+
+import (
+	"bufio"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func regexTest(calibrationvalues string) int {
+
+	scanner := bufio.NewScanner(strings.NewReader(calibrationvalues))
+
+	var sum = 0
+	var counter = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		sum += regexSum(line)
+		testSum := regexSum(line)
+		fmt.Printf("#%d: %s : %d\n", counter, line, testSum)
+		counter++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("error occurred: %v\n", err)
+	}
+	return sum
+}
+
+var regexnumerals = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func regexSum(line string) int {
+	r := regexp.MustCompile(`([1-9]|one|two|three|four|five|six|seven|eight|nine)`)
+
+	sum := 0
+	var matches []string
+	for i := 0; i < len(line); i++ {
+		// Finds the leftsmost match from the i: position
+		loc := r.FindStringIndex(line[i:])
+		if loc != nil {
+			// overlap matches
+			match := line[i:][loc[0]:loc[1]]
+
+			// Convert word numerals to their numeric counterparts
+			if num, ok := regexnumerals[match]; ok {
+				match = num
+			}
+			matches = append(matches, match)
+
+			// bump the index
+			i += loc[0]
+		}
+	}
+
+	sum, _ = strconv.Atoi(matches[0] + matches[len(matches)-1])
+
+	return sum
+}
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -154,3 +154,143 @@ func TestDay1PartTwoTestPuzzle(t *testing.T) {
 		t.Errorf("CalibrationValue() == %d, want %d", got, want)
 	}
 }
+
+func TestDayRegex1PartOneInitial(t *testing.T) {
+	input := `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
+	want := 142
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartOneInitial2(t *testing.T) {
+	input := `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
+	want := 142
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartTwoInitial1(t *testing.T) {
+	input := `two1nine`
+	want := 29
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartTwoInitial2(t *testing.T) {
+	input := `eightwothree`
+	want := 83
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartTwoInitial3(t *testing.T) {
+	input := `abcone2threexyz`
+	want := 13
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartTwoInitial4(t *testing.T) {
+	input := `xtwone3four`
+	want := 24
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartTwoInitial5(t *testing.T) {
+	input := `4nineeightseven2`
+	want := 42
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartTwoInitial6(t *testing.T) {
+	input := `zoneight234`
+	want := 14
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartTwoInitial7(t *testing.T) {
+	input := `7pqrstsixteen`
+	want := 76
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartTwoInitialAll(t *testing.T) {
+	input := `54`
+	want := 54
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+func TestDayRegex1PartTwoInitial8(t *testing.T) {
+	input := `374`
+	want := 34
+	got := regexTest(input)
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+func TestDayRegex1PartTwoTestingWithOffCases(t *testing.T) {
+	input := `eighthree
+oneight
+sevenine
+twone`
+	want := 201
+	got := regexTest(input)
+
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
+
+// I hate my life. Why is this test failing?
+func TestDayRegex1PartTwoTestPuzzle(t *testing.T) {
+
+	file, err := os.Open("puzzleinput")
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+
+	input := string(bytes)
+	want := 55413
+	got := regexTest(input)
+
+	if got != want {
+		t.Errorf("CalibrationValue() == %d, want %d", got, want)
+	}
+}
